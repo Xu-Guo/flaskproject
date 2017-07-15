c3ba5bdd0e365e56c66003323a11c8481ef1ff75
XuGuo <xuguo2002@gmail.com>
2017-07-15 15:28:03 -0700

    7/15

--- remote_control_robot/camera.py
+++ remote_control_robot/camera.py
@@ -1,24 +1,27 @@
-# camera.py
-
+from time import time
+from SimpleCV import Camera, Image, Color, Display
+import numpy as np
 import cv2
 
-class VideoCamera(object):
+
+class MyCamera(object):
+    """An emulated camera implementation that streams a repeated sequence of
+    files 1.jpg, 2.jpg and 3.jpg at a rate of one frame per second."""
+    lastTime = 0
     def __init__(self):
-        # Using OpenCV to capture from device 0. If you have trouble capturing
-        # from a webcam, comment the line below out and use a video file
-        # instead.
-        self.video = cv2.VideoCapture(0)
-        # If you decide to use video.mp4, you must have this file in the folder
-        # as the main.py.
-        # self.video = cv2.VideoCapture('video.mp4')
-    
-    def __del__(self):
-        self.video.release()
-    
+        self.usbCam = cv2.VideoCapture(0)
+    self.usbCam.set(3,400)
+    self.usbCam.set(4,300)
+    ret, frame = self.usbCam.read()
+    self.cache = cv2.imencode('.jpg',frame)[1].tostring()
+    #self.frames = [open(f + '.jpg', 'rb').read() for f in ['1', '2', '3']]
+
     def get_frame(self):
-        success, image = self.video.read()
-        # We are using Motion JPEG, but OpenCV defaults to capture raw images,
-        # so we must encode it into JPEG in order to correctly display the
-        # video stream.
-        ret, jpeg = cv2.imencode('.jpg', image)
-        return jpeg.tobytes()
+    diff = time()*1000 - self.lastTime;
+    #if diff<30 and self.cache!=None:
+    #   return self.cache
+    ret, frame = self.usbCam.read()
+    self.cache = cv2.imencode('.jpg',frame)[1].tostring()
+    self.lastTime = time()*1000
+    return self.cache
+    
