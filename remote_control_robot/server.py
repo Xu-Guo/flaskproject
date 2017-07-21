d9b42157b6f83052d268d2a7b248603f47d423fb
XuGuo <xuguo2002@gmail.com>
2017-07-21 15:07:47 -0700

    7721

--- remote_control_robot/server.py
+++ remote_control_robot/server.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from flask import Flask,render_template,Response,request,jsonify
 # emulated camera
-from camera import MyCamera
+# from camera import MyCamera
 from time import time
 # Raspberry Pi camera module (requires picamera package)
 # from camera_pi import Camera
@@ -20,17 +20,17 @@ def index():
 	return render_template('index.html')
 
 
-def gen(camera):
-	"""Video streaming generator function."""
-	while True:
-		frame = camera.get_frame()
-		yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
-
-@app.route('/video_feed')
-def video_feed():
-	"""Video streaming route. Put this in the src attribute of an img tag."""
-	return Response(gen(MyCamera()), mimetype='multipart/x-mixed-replace; boundary=frame')
+# def gen(camera):
+# 	"""Video streaming generator function."""
+# 	while True:
+# 		frame = camera.get_frame()
+# 		yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+#
+#
+# @app.route('/video_feed')
+# def video_feed():
+# 	"""Video streaming route. Put this in the src attribute of an img tag."""
+# 	return Response(gen(MyCamera()), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 bus = None
